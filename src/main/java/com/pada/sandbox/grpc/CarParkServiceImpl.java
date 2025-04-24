2f9b848072bec3db5039648e6011bc6f8c21578a
unknown <adamski-piotrek@wp.pl>
2025-04-24 21:45:05 +0200

    Implement gRPC based service

--- src/main/java/com/pada/sandbox/grpc/CarParkServiceImpl.java
+++ src/main/java/com/pada/sandbox/grpc/CarParkServiceImpl.java
@@ -1,10 +1,21 @@
 package com.pada.sandbox.grpc;
 
 
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import pada.testowe.GreetingServiceGrpc;
+import pada.testowe.ParkRequest;
+import pada.testowe.ParkResponse;
 
-// @GrpcService
+@GrpcService
 public class CarParkServiceImpl  extends GreetingServiceGrpc.GreetingServiceImplBase {
 
+    @Override
+    public void sayHello(ParkRequest request, StreamObserver<ParkResponse> responseObserver) {
+        System.out.println("Hi from gRPC");
+        ParkResponse response = ParkResponse.newBuilder().setResponseMessage("Hello gRPC").build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
 
